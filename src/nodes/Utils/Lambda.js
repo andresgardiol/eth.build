306063aa5de265a6d8e51c7d91ad1344c0efd3e6
Andres Gardiol <andresgardiol@gmail.com>
2021-07-07 00:19:09 -0300

    Add lambda

---
+++ src/nodes/Utils/Lambda.js
@@ -0,0 +1,33 @@
+const keccak256 = require("keccak256");
+
+function Lambda() {
+  this.addInput("trigger", -1);
+  this.addOutput("message", "string");
+  this.addOutput("finish", -1);
+  let id = keccak256(getToLocaleTimeString()).toString("hex").substr(0, 9);
+  this.properties = { executionTime: 2000, id };
+  this.value = "ok";
+  this.size = [160, 60];
+}
+
+Lambda.title = "Lambda";
+
+Lambda.prototype.onExecute = function() {
+};
+
+Lambda.prototype.onAction = async function() {
+  console.log(`%cSTART - LAMBDA ID: ${this.properties.id} - ${getToLocaleTimeString()}`, "color: green");
+  setTimeout(() => {
+    this.setOutputData(0, this.value);
+    this.trigger("finish", "");
+    console.log(`%cEND - LAMBDA ID: ${this.properties.id} - ${getToLocaleTimeString()}`, "color: green");
+    console.log(`%cREPORT - LAMBDA TOOK: ${this.properties.executionTime}ms TO COMPLETE`, "color: orange");
+  }, this.properties.executionTime);
+
+};
+
+function getToLocaleTimeString() {
+  return new Date().toLocaleTimeString();
+}
+
+export default Lambda;
