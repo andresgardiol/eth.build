378a1d1c794a0ae754c24645440904f0a77b7e80
Andres Gardiol <andresgardiol@gmail.com>
2021-07-18 18:54:30 -0300

    Remove unused components
    - Update about dialog
    - Add firebase deploy config

--- src/App.js
+++ src/App.js
@@ -193,17 +193,13 @@ function App() {
         <Divider/>
         <CardActions style={{justifyContent: 'center'}}>
           <div style={{padding:"2%"}}>
-            <a target="_blank" href="https://eth.build">Eth.Build</a> (<a target="_blank" href="https://github.com/austintgriffith/eth.build">source</a>) created by <a target="_blank" href="https://twitter.com/austingriffith">Austin Griffith</a>
+            <a target="_blank" href="https://eth.build">AWS POC</a> (<a target="_blank" href="https://github.com/andresgardiol/eth.build">source</a>) its a fork of <a target="_blank" href="https://github.com/austintgriffith/eth.build">Austin Griffith eth.build project</a>
           </div>
         </CardActions>
-        <CardActions style={{justifyContent: 'center'}}>
-          <div style={{padding:"2%"}}>
-            With support from <a target="_blank" href="https://ethereum.org">the Ethereum Foundation</a>, <a target="_blank" href="https://consensys.net/">Consensys</a>, and <a target="_blank" href="https://gitcoin.co/grants/122/austin-griffith-ethereum-rampd">Gitcoin Grants</a>
-        </div>
-      </CardActions>
+
       <CardActions style={{justifyContent: 'center'}}>
         <div style={{padding:"2%"}}>
-          Special thanks to <a target="_blank" href="https://github.com/jagenjo">Javi Agenjo</a> for <a target="_blank" href="https://github.com/jagenjo/litegraph.js">litegraph</a>
+
       </div>
     </CardActions>
   </Dialog>
@@ -1077,9 +1073,9 @@ return (
           <span onClick={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
             onTouchStart={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
           >
-            <span style={{color:"#03a9f4"}}>eth</span>
+            <span style={{color:"#03a9f4"}}>AWS</span>
             <span style={{position:'relative',left:-5,bottom:15,color:"#f44336",marginBottom:25}}>.</span>
-            <span style={{position:'relative',left:-10,color:"#333"}}>build</span>
+            <span style={{position:'relative',left:-10,color:"#333"}}>POC</span>
           </span>
 
           <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={()=>{
@@ -1118,16 +1114,6 @@ return (
               <svg style={{width:24,height:24,opacity:0.95}} viewBox="0 0 24 24">
                 <path fill="#000000" d="M4,4H10V10H4V4M20,4V10H14V4H20M14,15H16V13H14V11H16V13H18V11H20V13H18V15H20V18H18V20H16V18H13V20H11V16H14V15M16,15V18H18V15H16M4,20V14H10V20H4M6,6V8H8V6H6M16,6V8H18V6H16M6,16V18H8V16H6M4,11H6V13H4V11M9,11H13V15H11V13H9V11M11,6H13V10H11V6M2,2V6H0V2A2,2 0 0,1 2,0H6V2H2M22,0A2,2 0 0,1 24,2V6H22V2H18V0H22M2,18V22H6V24H2A2,2 0 0,1 0,22V18H2M22,22V18H24V22A2,2 0 0,1 22,24H18V22H22Z" />
               </svg>
-            </Tooltip>
-          </span>
-
-          <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
-              setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true)
-            }}>
-            <Tooltip title="Learn More" style={{marginLeft:10,cursor:"pointer"}}>
-              <Icon>
-                swap_vert
-              </Icon>
             </Tooltip>
           </span>
 
